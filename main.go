3561cb79d976eab881f9c5c784fbe0f21177048e
Patrick Hachicho <hachichopatrick@gmail.com>
2024-04-29 13:19:05 -0400

    added errors for paths

--- main.go
+++ main.go
@@ -6,8 +6,16 @@ import (
 )
 
 // Define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+// Check if the current request URL path exactly matches "/". If it doesn't, use 
+// the http.NotFound() function to send a 404 response to the client.
+// Importantly, we then return from the handler. If we don't return the handler 
+// would keep executing and also write the "Hello from SnippetBox" message.
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+		}
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -16,7 +24,16 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	// check using "curl -i -X POST http://localhost:4000/snippet/create"
+	if r.Method != "POST" {
+		w.WriteHeader(405)
+		w.Write([]byte("Method Not Allowed"))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet"))
+
 }
 
 func main(){
